74bc08bc35386aa800935b6555dbb97dd377b3e5
HafsaEch <hafsa.chryf137@gmail.com>
2022-01-04 22:45:06 +0100

    update slider

--- src/components/MySlider.vue
+++ src/components/MySlider.vue
@@ -1,13 +1,13 @@
 <template>
 <div>
-      <!-- <div class="flex ">
+      <div class="flex ">
           <div class="bg-gray-200 w-12  rounded-2xl h-12 flex items-center justify-center cursor-pointer mr-4">
           </div>
           <div @click="next" class="bg-Green w-12 rounded-2xl h-12 flex items-center justify-center cursor-pointer">
           </div>
-        </div> -->
-        <div class="flex mb-36 overflow-hidden " style="height:340px; width: 1311px;" :style="{transform:`translateX(${index}px)`}">
-            <div v-for="(item,index) in sources" :key='index' class="bg-Blue mr-6 p-6 rounded-xl" >
+        </div>
+        <div class="flex mb-36 overflow-hidden " style="height:340px; width: 1311px;" >
+            <div v-for="(item) in sources" :key='item.id' class="bg-Blue mr-6 p-6 rounded-xl" :style="{transform:`translateX(${index}px)`}">
 
                     <img  :src="item.source" alt="" class="rounded-xl w-96 h-60">
                     <div class="flex items-center justify-evently my-3">
@@ -30,11 +30,15 @@ export default {
                
             ],
             sources:[
-                {source:'img/casa.jpg'},
-                {source:'img/alhoceima.jpeg'},
-                {source:'img/casti.jpg'},
-                {source:'img/fes.jpg'},
-                {source:'img/marakesh.jpg'},
+                {id:1, source:'img/casa.jpg'},
+                {id:2, source:'img/alhoceima.jpeg'},
+                {id:3, source:'img/casti.jpg'},
+                {id:4, source:'img/fes.jpg'},
+                {id:5, source:'img/marakesh.jpg'},
+                {id:6, source:'img/Hespress.png'},
+                {id:7, source:'img/Image.png'},
+                {id:8, source:'img/logo.png'},
+                {id:9, source:'img/pressT.png'},
             ]
        }
     },
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,100 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="slider">
+    <button @click="prev()" type="button" class="btn btn-left">
+      <myicons name="left"  class="text-white  icone"></myicons>
+    </button>
+    <div class="container-slides" :style="{transform:`translateX(${index}px)`}">
+      <img src="img/alhoceima.jpeg" alt="" class="img-slider">
+      <img src="img/casa.jpg" alt="" class="img-slider">
+      <img src="img/casti.jpg" alt="" class="img-slider">
+      <img src="img/fes.jpg" alt="" class="img-slider">
+    </div>
+     <button @click="next()" type="button"  class="btn btn-right">
+      <myicons name="right" class="text-white icone"></myicons>
+    </button>
   </div>
 </template>
+<script>
+import Myicons from '@/components/Myicons.vue'
+
+export default {
+  components:{
+    Myicons
+  },
+  data(){
+    return{
+      index:0
+    }
+  },
+  methods:{
+    next(){
+      console.log(this.index)
+      if(this.index === -1500){
+        this.index= 0
+      }
+      else{
+        this.index -= 500;
+      }
+    },
+    prev(){
+      console.log(this.index)
+
+      if(this.index === 0){
+        this.index = -1500
+      }
+      else{
+        this.index += 500
+      }
+    }
+  }
+
+  
+}
+</script>
+<style scoped>
+  .slider{
+    width: 500px;
+    height: 300px;
+    margin: 100px auto 0;
+    overflow: hidden;
+    position: relative;
+  }
+  .container-slides{
+    display: flex;
+  }
+  .img-slider{
+    width: 100%;
+    height: auto;
+  }
+  .btn{
+    background-color: aqua;
+    outline: none;
+    border: none;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    display: block;
+    position: absolute;
+    z-index: 1000;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items:center ;
+  }
+  .icone{
+    width:15px;
+    
+  }
+  .btn-left{
+    top: 50%;
+    left: 5px;
+    transform: translateY(-50%);
+  }
+   .btn-right{
+    top: 50%;
+    right: 5px;
+    transform: translateY(-50%);
+  }
+  
+</style>
+
